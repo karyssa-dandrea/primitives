e6e11c1810c36d7399294053a59c21ce94b8daad
karyssa-dandrea <karyssadandrea@gmail.com>
2022-04-04 10:20:46 -0700

    reverse words challenege done

---
+++ Primitive.js
@@ -0,0 +1,12 @@
+const string = 'Josh is super cool';
+
+function reverseWords(sentence) {
+  return sentence
+    .split(' ')
+    .map((word) => {
+      return word.split('').reverse().join('');
+    })
+    .join(' ');
+}
+
+console.log(reverseWords(string));
