3ccf725b9028fd2ed4edcc04c8b5de767cc833a1
karyssa-dandrea <karyssadandrea@gmail.com>
2022-04-04 12:51:36 -0700

    got fizzbuzz challenge

--- Primitive.js
+++ Primitive.js
@@ -34,3 +34,45 @@ function oddishOrEvenish(number) {
 }
 
 console.log(oddishOrEvenish(number));
+
+// partner 2 - title Case
+function titleCase(sentence) {
+  return sentence
+    .split(' ')
+    .map((word) => {
+      return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    })
+    .join(' ');
+}
+
+const newTItle = titleCase('alchemy ROCKS goLD');
+
+//array at
+function at(arr, index) {
+  if (index > -1) {
+    return arr[index];
+  } else {
+    return arr[arr.length + index];
+  }
+}
+
+function fizzBuzz(number) {
+  const numbersArr = [];
+  for (let i = 1; number >= i; i++) {
+    numbersArr.push(i);
+    if (i % 3 === 0 && i % 5 === 0) {
+      numbersArr.pop();
+      numbersArr.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      numbersArr.pop();
+      numbersArr.push('Fizz');
+    } else if (i % 5 === 0) {
+      numbersArr.pop();
+      numbersArr.push('Buzz');
+    }
+  }
+  return numbersArr;
+}
+fizzBuzz(16);
+
+console.log(fizzBuzz(16));
